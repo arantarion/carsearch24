98f787f1a5ae70b5a9cb6eb8e98e287e74d3b310
Anton <anton.drees@smail.inf.h-brs.de>
2020-08-29 10:17:31 +0200

    Finished getRole in LoginControl

--- src/main/java/org/bonn/se2/process/control/LoginControl.java
+++ src/main/java/org/bonn/se2/process/control/LoginControl.java
@@ -1,14 +1,21 @@
 package org.bonn.se2.process.control;
 
 import com.vaadin.ui.UI;
+import org.bonn.se2.model.dao.CustomerDAO;
+import org.bonn.se2.model.dao.SalesmanDAO;
 import org.bonn.se2.model.dao.UserDAO;
+import org.bonn.se2.model.objects.dto.Salesman;
 import org.bonn.se2.model.objects.dto.User;
 import org.bonn.se2.model.objects.dto.UserAtLogin;
 import org.bonn.se2.process.control.exceptions.DatabaseException;
 import org.bonn.se2.process.control.exceptions.InvalidCredentialsException;
+import org.bonn.se2.services.util.Config;
 import org.bonn.se2.services.util.CryptoFunctions;
 import org.bonn.se2.services.util.SessionFunctions;
 import org.bonn.se2.services.util.UIFunctions;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class LoginControl {
 
@@ -32,7 +39,19 @@ public class LoginControl {
     }
 
     public static String getRole(User user) {
-        return "undefined";
+        try {
+            new CustomerDAO().retrieve(user.getUserID());
+            return Config.Roles.CUSTOMER;
+
+        } catch (DatabaseException e) {
+            try {
+                new SalesmanDAO().retrieve(user.getUserID());
+                return Config.Roles.SALESMAN;
+            } catch (DatabaseException ex) {
+                Logger.getLogger(LoginControl.class.getName()).log(Level.SEVERE, null, ex);
+                return null;
+            }
+        }
     }
 
 }
