737db0ae03944f33ed2571842e2ae1cf3711166c
Henry Weckermann <henry.weckermann@smail.inf.h-brs.de>
2020-08-31 15:03:48 +0200

    some fixes and better looks

--- src/main/java/org/bonn/se2/gui/windows/CarCreationWindow.java
+++ src/main/java/org/bonn/se2/gui/windows/CarCreationWindow.java
@@ -1,5 +1,8 @@
 package org.bonn.se2.gui.windows;
 
+import com.vaadin.data.Binder;
+import com.vaadin.data.converter.StringToIntegerConverter;
+import com.vaadin.data.validator.EmailValidator;
 import com.vaadin.server.Page;
 import com.vaadin.shared.Position;
 import com.vaadin.ui.*;
@@ -7,64 +10,95 @@ import org.bonn.se2.model.dao.CarDAO;
 import org.bonn.se2.model.dao.SalesmanDAO;
 import org.bonn.se2.model.objects.dto.Car;
 import org.bonn.se2.model.objects.dto.Salesman;
+import org.bonn.se2.model.objects.dto.User;
 import org.bonn.se2.process.control.exceptions.DatabaseException;
 import org.bonn.se2.services.util.SessionFunctions;
 
 import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class CarCreationWindow extends Window {
 
-    public CarCreationWindow(){
+    private final Binder<Car> carBinder = new Binder<>();
+
+    public CarCreationWindow() {
         this.setUp();
     }
 
-    private void setUp(){
+    private void setUp() {
         Label title = new Label("Füllen Sie bitte alle Felder aus.");
         VerticalLayout creationLayout = new VerticalLayout();
-        Button Speichern = new Button("Speichern");
+        Button saveButton = new Button("Speichern");
+        saveButton.setSizeFull();
         TextField brand = new TextField("Marke");
+        brand.setSizeFull();
         TextField buildYear = new TextField("Baujahr");
+        buildYear.setSizeFull();
         TextArea description = new TextArea("Beschreibung");
+        description.setSizeFull();
         TextField color = new TextField("Farbe");
+        color.setSizeFull();
         TextField price = new TextField("Preis");
+        price.setSizeFull();
         TextField model = new TextField("Modell");
+        model.setSizeFull();
 
-        creationLayout.addComponents(title,brand,buildYear,description,color,price,model,Speichern);
+        creationLayout.addComponents(title, brand, model, buildYear, color, description, price, saveButton);
+
         this.setContent(creationLayout);
         this.setWidth("17%");
         this.center();
 
-        Speichern.addClickListener(new Button.ClickListener() {
-            @Override
-            public void buttonClick(Button.ClickEvent clickEvent) {
-                String Marke = brand.getValue();
-                Integer Baujahr = Integer.parseInt(buildYear.getValue());
-                String Beschreibung = description.getValue();
-                String Farbe = color.getValue();
-                String Preis = price.getValue();
-                String Modell = model.getValue();
-                Integer userID = SessionFunctions.getCurrentUser().getUserID();
-                Integer salesmanID = null;
-                try {
-                    SalesmanDAO salesmanDAO = new SalesmanDAO();
-                    Salesman salesman = salesmanDAO.retrieve(userID);
-                    salesmanID = salesman.getSalesmanID();
-                } catch (DatabaseException e) {
-                    e.printStackTrace();
-                }
+        Label nameStatus = new Label();
 
-                Car dto = new Car(Baujahr,Preis,Marke,Beschreibung,Farbe,Modell,salesmanID);
-                try {
-                    CarDAO carDao = new CarDAO();
-                    carDao.create(dto);
-                    close();
-                    Notification notification = new Notification("Sie haben erfolgreich ein Auto erstellt.", Notification.Type.ASSISTIVE_NOTIFICATION);
-                    notification.setPosition(Position.BOTTOM_CENTER);
-                    notification.setDelayMsec(4000);
-                    notification.show(Page.getCurrent());
-                } catch (DatabaseException | SQLException e) {
-                    e.printStackTrace();
-                }
+        carBinder.forField(buildYear)
+                .withConverter(new StringToIntegerConverter("Bitte geben Sie ein gültiges Jahr an!"))
+                .withValidator(year -> year >= 1900 && year < 2100, "Bitte geben Sie ein gültiges Jahr an!")
+                .bind(Car::getBuildYear, Car::setBuildYear);
+
+
+        saveButton.addClickListener((Button.ClickListener) clickEvent -> {
+            String Marke = brand.getValue();
+            String Beschreibung = description.getValue();
+            String Farbe = color.getValue();
+            String Preis = price.getValue();
+            String Modell = model.getValue();
+            Integer userID = SessionFunctions.getCurrentUser().getUserID();
+            Integer salesmanID = null;
+            Integer Baujahr = null;
+
+            try {
+                Baujahr = Integer.parseInt(buildYear.getValue());
+            } catch (NumberFormatException nve) {
+                Notification notification = new Notification("Kein gültiges Jahr.", Notification.Type.ERROR_MESSAGE);
+                notification.setPosition(Position.BOTTOM_CENTER);
+                notification.setDelayMsec(4000);
+                notification.show(Page.getCurrent());
+                return;
+            }
+
+            try {
+                SalesmanDAO salesmanDAO = new SalesmanDAO();
+                Salesman salesman = salesmanDAO.retrieve(userID);
+                salesmanID = salesman.getSalesmanID();
+            } catch (DatabaseException e) {
+                Logger.getLogger(this.getClass().getSimpleName()).log(Level.SEVERE,
+                        new Throwable().getStackTrace()[0].getMethodName() + " failed", e);
+            }
+
+            Car dto = new Car(Baujahr, Preis, Marke, Beschreibung, Farbe, Modell, salesmanID);
+            try {
+                CarDAO carDao = new CarDAO();
+                carDao.create(dto);
+                close();
+                Notification notification = new Notification("Sie haben erfolgreich ein Auto erstellt.", Notification.Type.ASSISTIVE_NOTIFICATION);
+                notification.setPosition(Position.BOTTOM_CENTER);
+                notification.setDelayMsec(4000);
+                notification.show(Page.getCurrent());
+            } catch (DatabaseException | SQLException e) {
+                Logger.getLogger(this.getClass().getSimpleName()).log(Level.SEVERE,
+                        new Throwable().getStackTrace()[0].getMethodName() + " failed", e);
             }
         });
     }
