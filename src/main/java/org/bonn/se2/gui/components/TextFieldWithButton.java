6f403246c93812700a49d3116d8ba1d7ec22f210
Henry Weckermann <henry.weckermann@smail.inf.h-brs.de>
2020-09-01 18:01:36 +0200

    Made a new searchbar and added button functionality

---
+++ src/main/java/org/bonn/se2/gui/components/TextFieldWithButton.java
@@ -0,0 +1,42 @@
+package org.bonn.se2.gui.components;
+
+import com.vaadin.icons.VaadinIcons;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.CssLayout;
+import com.vaadin.ui.TextField;
+import com.vaadin.ui.themes.ValoTheme;
+import eu.maxschuster.vaadin.autocompletetextfield.AutocompleteTextField;
+
+public class TextFieldWithButton extends CssLayout {
+
+    private final AutocompleteTextField textField;
+    private final Button button;
+
+    public TextFieldWithButton() {
+        setStyleName(ValoTheme.LAYOUT_COMPONENT_GROUP);
+
+        textField = new AutocompleteTextField();
+        textField.setPlaceholder("Suchen...");
+        textField.addStyleName("inline-icon");
+        textField.setIcon(VaadinIcons.SEARCH);
+        textField.setWidth("600px");
+
+
+
+        button = new Button(VaadinIcons.CLOSE_SMALL);
+        button.addStyleNames(ValoTheme.BUTTON_ICON_ONLY, ValoTheme.BUTTON_BORDERLESS);
+
+        button.addClickListener(e -> textField.setValue(""));
+
+        addComponents(textField, button);
+    }
+
+    public AutocompleteTextField getTextField() {
+        return textField;
+    }
+
+    public Button getButton() {
+        return button;
+    }
+
+}
--- src/main/java/org/bonn/se2/gui/view/MainView.java
+++ src/main/java/org/bonn/se2/gui/view/MainView.java
@@ -3,24 +3,29 @@ package org.bonn.se2.gui.view;
 import com.vaadin.icons.VaadinIcons;
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener;
+import com.vaadin.server.Page;
+import com.vaadin.shared.Position;
 import com.vaadin.shared.ui.ContentMode;
 import com.vaadin.shared.ui.grid.HeightMode;
 import com.vaadin.ui.*;
+import com.vaadin.ui.themes.ValoTheme;
 import eu.maxschuster.vaadin.autocompletetextfield.AutocompleteSuggestionProvider;
 import eu.maxschuster.vaadin.autocompletetextfield.AutocompleteTextField;
 import eu.maxschuster.vaadin.autocompletetextfield.provider.CollectionSuggestionProvider;
 import eu.maxschuster.vaadin.autocompletetextfield.provider.MatchMode;
 import eu.maxschuster.vaadin.autocompletetextfield.shared.ScrollBehavior;
 import org.bonn.se2.gui.components.NavigationBar;
+import org.bonn.se2.gui.components.TextFieldWithButton;
 import org.bonn.se2.gui.windows.CarCreationWindow;
 import org.bonn.se2.model.dao.CarDAO;
 import org.bonn.se2.model.objects.dto.Car;
 import org.bonn.se2.process.control.exceptions.DatabaseException;
 import org.bonn.se2.services.util.Config;
+import org.bonn.se2.services.util.Notifier;
 import org.bonn.se2.services.util.SessionFunctions;
 
 import java.sql.SQLException;
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 import java.util.Locale;
@@ -28,6 +33,8 @@ import java.util.stream.Collectors;
 
 
 public class MainView extends VerticalLayout implements View {
+
+    protected Car selectedCar = null;
 
     @Override
     public void enter(ViewChangeListener.ViewChangeEvent event) {
@@ -58,36 +65,24 @@ public class MainView extends VerticalLayout implements View {
 
         //Button suche = new Button("Suchen", VaadinIcons.SEARCH);
         Button carCreation = new Button("Auto erstellen", VaadinIcons.CAR);
-        TextField searchField = new TextField();
-        searchField.setPlaceholder("Suchen...");
-        searchField.addStyleName("inline-icon");
-        searchField.setIcon(VaadinIcons.SEARCH);
-        searchField.setWidth("500px");
 
         AutocompleteTextField autoSearchField = new AutocompleteTextField();
         autoSearchField.setPlaceholder("Suchen...");
         autoSearchField.addStyleName("inline-icon");
         autoSearchField.setIcon(VaadinIcons.SEARCH);
-        autoSearchField.setWidth("500px");
+        autoSearchField.setWidth("600px");
 
-        //Label label = new Label("Bitte geben Sie ein Stichwort ein:");
-
-//        addComponent(horizontalLayout);
-//        setComponentAlignment(horizontalLayout, Alignment.TOP_RIGHT);
-//
-//        addComponent(h2);
-//        setComponentAlignment(h2, Alignment.TOP_LEFT);
-//
-//        addComponent(horizontalLayoutCompany);
+        Button deleteSearchButton = new Button("x");
+        deleteSearchButton.addStyleNames(ValoTheme.BUTTON_BORDERLESS, ValoTheme.BUTTON_TINY);
 
         addComponents(horizontalLayout, h2, horizontalLayoutCompany);
         setComponentAlignment(horizontalLayout, Alignment.TOP_RIGHT);
         setComponentAlignment(h2, Alignment.TOP_LEFT);
         setComponentAlignment(horizontalLayoutCompany, Alignment.MIDDLE_CENTER);
 
-        //horizontalLayoutCompany.addComponent(label);
-        horizontalLayoutCompany.addComponent(searchField);
-        horizontalLayoutCompany.addComponent(autoSearchField);
+        //horizontalLayoutCompany.addComponents(autoSearchField, deleteSearchButton);
+        TextFieldWithButton fieldWithButton = new TextFieldWithButton();
+        horizontalLayoutCompany.addComponent(fieldWithButton);
         horizontalLayoutCompany.addComponent(new Label("&nbsp", ContentMode.HTML));
 
         if (SessionFunctions.getCurrentRole().equals(Config.Roles.SALESMAN)) {
@@ -100,11 +95,11 @@ public class MainView extends VerticalLayout implements View {
         });
 
         Grid<Car> grid = new Grid<>();
-
         CarDAO carDAO = new CarDAO();
         List<Car> liste = carDAO.retrieveAll();
 
         grid.setSizeFull();
+
         grid.setHeightMode(HeightMode.UNDEFINED);
 
         grid.setItems(liste);
@@ -113,23 +108,25 @@ public class MainView extends VerticalLayout implements View {
         this.addComponent(grid);
         this.setComponentAlignment(grid, Alignment.MIDDLE_CENTER);
 
+        Collection<String> suggestions = liste.stream()
+                .map(Car::toString)
+                .map(w -> w.split("\\s+"))
+                .flatMap(Arrays::stream)
+                .distinct()
+                .collect(Collectors.toList());
 
-        Collection<String> suggestions = liste.stream().map(Car::toString).collect(Collectors.toList());
+        AutocompleteSuggestionProvider suggestionProvider = new CollectionSuggestionProvider(suggestions, MatchMode.BEGINS, true, Locale.GERMAN);
 
-        AutocompleteSuggestionProvider suggestionProvider = new CollectionSuggestionProvider(suggestions, MatchMode.CONTAINS, true, Locale.GERMAN);
+        fieldWithButton.getTextField().setDelay(150);
+        fieldWithButton.getTextField().setItemAsHtml(false);
+        fieldWithButton.getTextField().setMinChars(3);
+        fieldWithButton.getTextField().setScrollBehavior(ScrollBehavior.NONE);
+        fieldWithButton.getTextField().setSuggestionProvider(suggestionProvider);
 
-
-        autoSearchField.setDelay(150); // Delay before sending a query to the server
-        autoSearchField.setItemAsHtml(false); // Suggestions contain html formating. If true, make sure that the html is save to use!
-        autoSearchField.setMinChars(3); // The required value length to trigger a query
-        autoSearchField.setScrollBehavior(ScrollBehavior.NONE); // The method that should be used to compensate scrolling of the page
-        autoSearchField.setSuggestionProvider(suggestionProvider);
-
-        //searchField.addValueChangeListener(e -> {
-        autoSearchField.addValueChangeListener(e -> {
+        fieldWithButton.getTextField().addValueChangeListener(e -> {
             if (!autoSearchField.getValue().equals("")) {
-                grid.removeAllColumns();
 
+                grid.removeAllColumns();
                 String attr = autoSearchField.getValue();
                 List<Car> liste2 = null;
                 try {
@@ -137,20 +134,17 @@ public class MainView extends VerticalLayout implements View {
                 } catch (DatabaseException | SQLException databaseException) {
                     databaseException.printStackTrace();
                 }
-
                 grid.setItems(liste2);
 
             } else {
+
                 grid.removeAllColumns();
-
                 List<Car> liste3 = null;
-
                 try {
                     liste3 = carDAO.retrieveAll();
                 } catch (DatabaseException databaseException) {
                     databaseException.printStackTrace();
                 }
-
                 grid.setItems(liste3);
             }
             addGridComponents(grid);
@@ -158,19 +152,25 @@ public class MainView extends VerticalLayout implements View {
 
     }
 
-
     private void addGridComponents(Grid<Car> grid) {
         grid.addColumn(Car::getBrand).setCaption("Marke");
         grid.addColumn(Car::getModel).setCaption("Modell");
         grid.addColumn(Car::getBuildYear).setCaption("Baujahr");
         grid.addColumn(Car::getColor).setCaption("Farbe");
-        grid.addColumn(Car::getDescription).setCaption("Beschreibung");
+        grid.addColumn(Car::getDescription).setCaption("Beschreibung").setSortable(false);
         grid.addColumn(Car::getPrice).setCaption("Preis");
-        grid.addComponentColumn(jobOffer -> {
+        grid.addComponentColumn(car -> {
             Button button;
             button = new Button("Reservieren");
             button.addClickListener(click -> {
-                Notification.show("Reserviert");
+                if (SessionFunctions.getCurrentRole().equals(Config.Roles.CUSTOMER)) {
+                    System.out.println(car);
+                } else {
+                    Notification noti = new Notification("Nur Kunden können ein Auto reservieren");
+                    noti.setDelayMsec(3000);
+                    noti.setPosition(Position.BOTTOM_CENTER);
+                    noti.show(Page.getCurrent());
+                }
             });
             return button;
         }).setCaption("Reservieren");
