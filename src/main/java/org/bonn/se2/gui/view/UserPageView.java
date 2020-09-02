5cf9a2a2417d8910afc3359d72d0a8a65a2f12a6
Henry Weckermann <henry.weckermann@smail.inf.h-brs.de>
2020-09-02 19:06:08 +0200

    finalized the userpage

--- src/main/java/org/bonn/se2/gui/view/UserPageView.java
+++ src/main/java/org/bonn/se2/gui/view/UserPageView.java
@@ -1,5 +1,6 @@
 package org.bonn.se2.gui.view;
 
+import com.vaadin.icons.VaadinIcons;
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener;
 import com.vaadin.shared.ui.ContentMode;
@@ -7,10 +8,7 @@ import com.vaadin.shared.ui.grid.HeightMode;
 import com.vaadin.ui.*;
 import com.vaadin.ui.themes.ValoTheme;
 import org.bonn.se2.gui.components.NavigationBar;
-import org.bonn.se2.model.dao.CarDAO;
-import org.bonn.se2.model.dao.CustomerDAO;
-import org.bonn.se2.model.dao.SalesmanDAO;
-import org.bonn.se2.model.dao.UserDAO;
+import org.bonn.se2.model.dao.*;
 import org.bonn.se2.model.objects.dto.Car;
 import org.bonn.se2.model.objects.dto.Customer;
 import org.bonn.se2.model.objects.dto.Salesman;
@@ -19,8 +17,10 @@ import org.bonn.se2.process.control.exceptions.DatabaseException;
 import org.bonn.se2.process.control.exceptions.InvalidCredentialsException;
 import org.bonn.se2.services.util.Config;
 import org.bonn.se2.services.util.SessionFunctions;
+import org.bonn.se2.services.util.UIFunctions;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -60,35 +60,55 @@ public class UserPageView extends VerticalLayout implements View {
         deleteButton.setEnabled(false);
         deleteButton.addStyleName("danger");
 
+        Button backButton = new Button("Zurück", e -> {
+            UIFunctions.gotoMain();
+        });
+        backButton.setIcon(VaadinIcons.ARROW_LEFT);
+
         VerticalLayout spacer = new VerticalLayout();
         Button spaceButton = new Button();
         spaceButton.setStyleName(ValoTheme.BUTTON_BORDERLESS);
         spacer.addComponents(spaceButton, spaceButton, spaceButton, spaceButton);
+
 
         if (SessionFunctions.getCurrentRole().equals(Config.Roles.CUSTOMER)) {
 
             CustomerDAO customerDAO = new CustomerDAO();
             customer = customerDAO.retrieve(user.getUserID());
 
-            //TODO HIER
+            ReservationDAO reservationDAO = new ReservationDAO();
+            List<Integer> resIDs = reservationDAO.retrieveReservationsByCustomerID(customer.getCustomerID());
 
-            Grid<Car> gridReservation = new Grid<>();
-            gridReservation.setSizeFull();
-            gridReservation.setHeightMode(HeightMode.UNDEFINED);
-            gridReservation.setSelectionMode(Grid.SelectionMode.NONE);
+            if (!(resIDs == null)) {
+                List<Car> reservedCars = new ArrayList<>();
 
-            CarDAO carDAO = new CarDAO();
+                Grid<Car> gridReservation = new Grid<>();
+                gridReservation.setSizeFull();
+                gridReservation.setHeightMode(HeightMode.UNDEFINED);
+                gridReservation.setSelectionMode(Grid.SelectionMode.NONE);
 
-            //TODO EHRE
-            List<Car> liste = carDAO.retrieveAll();
+                CarDAO carDAO = new CarDAO();
 
-            gridReservation.setItems(liste);
-            addGridComponentsCar(gridReservation);
+                for (int id : resIDs) {
+                    reservedCars.add(carDAO.retrieve(id));
+                }
 
-            this.addComponents(gridReservation);
-            this.setComponentAlignment(gridReservation, Alignment.MIDDLE_CENTER);
-            this.setComponentAlignment(deleteButton, Alignment.MIDDLE_CENTER);
+                gridReservation.setItems(reservedCars);
+                addGridComponentsCar(gridReservation);
 
+                this.addComponents(gridReservation, backButton);
+                this.setComponentAlignment(gridReservation, Alignment.MIDDLE_CENTER);
+
+            } else {
+                Label errNotice = new Label("<h2>Sie haben noch keine Reservierungen gemacht. <br>Sie können auf der Startseite nach Autos suchen und diese für einen Termin reservieren</h2>", ContentMode.HTML);
+                errNotice.setStyleName(ValoTheme.LABEL_FAILURE);
+
+                this.addComponent(errNotice);
+                this.setComponentAlignment(errNotice, Alignment.MIDDLE_CENTER);
+
+                this.addComponent(backButton);
+                this.setComponentAlignment(backButton, Alignment.MIDDLE_CENTER);
+            }
 
         } else {
 
@@ -106,9 +126,13 @@ public class UserPageView extends VerticalLayout implements View {
             gridCars.setItems(liste);
             addGridComponentsCar(gridCars);
 
-            this.addComponents(spacer, gridCars, deleteButton);
+            HorizontalLayout buttonLayout = new HorizontalLayout();
+            buttonLayout.addComponents(backButton, deleteButton);
+            buttonLayout.setComponentAlignment(deleteButton, Alignment.MIDDLE_CENTER);
+            buttonLayout.setComponentAlignment(backButton, Alignment.MIDDLE_LEFT);
+
+            this.addComponents(spacer, gridCars, buttonLayout);
             this.setComponentAlignment(gridCars, Alignment.MIDDLE_CENTER);
-            this.setComponentAlignment(deleteButton, Alignment.MIDDLE_CENTER);
 
             deleteButton.addClickListener(e -> {
                 try {
